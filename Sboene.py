6e540edb6cd351f43a2f82fe66ab715b61b05937
Priya <66324139+Priya-bioinfo@users.noreply.github.com>
2021-08-03 18:52:06 +0530

    Update Sboene.py

--- Sboene.py
+++ Sboene.py
@@ -1,4 +1,4 @@
-print("Name: Simone Salvador Boene")
-print("Email: simonboene@gmail.com")
-print("Slackname: @Sboene")
-print("Biostack: Genomics")
+print("Name:Simone Salvador Boene")
+print("Email:simonboene@gmail.com")
+print("Slackname:@Sboene")
+print("Biostack:Genomics")
