24ca17e7dea9fa023377bad3b8c557c31eb36612
Bhushan-Wagh025 <88301940+Bhushan-Wagh025@users.noreply.github.com>
2021-08-04 21:27:33 +0530

    Add files via upload

---
+++ Stage_0_XR.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main() 
+{ 
+printf("NAME: BHUSHAN WAGH \nEmail: waghbhushan1123@gmail.com\nLanguage_used: C_Program\nBiostack: Software Development \nSlack_username: @XR"); 
+return 0;
+}
