d8d40c0d103de816a47a48e67e3375acb8854a0a
Nisha6616 <72332145+Nisha6616@users.noreply.github.com>
2021-08-04 20:30:28 +0530

    Delete nisha.pl

--- nisha.pl
+++
@@ -1,6 +0,0 @@
-$name = 'Nisha Pujari';
-$Email = 'nishapujari6616@gmail.com';
-$username = 'Nisha';
-$Biostack = 'data analytics';
-
-print "\nName= $name\nEmail = $Email\nSlack_username= $username\nBiostack = $Biostack";
