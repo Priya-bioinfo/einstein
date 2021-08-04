f0ed32c5e518c55e9e44621bb75f8f6d86826373
Nisha6616 <72332145+Nisha6616@users.noreply.github.com>
2021-08-04 18:43:55 +0530

    Add files via upload

---
+++ nisha.pl
@@ -0,0 +1,6 @@
+$name = 'Nisha Pujari';
+$Email = 'nishapujari6616@gmail.com';
+$username = 'Nisha';
+$Biostack = 'data analytics';
+
+print "\nName= $name\nEmail = $Email\nSlack_username= $username\nBiostack = $Biostack";
