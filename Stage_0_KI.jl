8a11e6fae4153e0740f5bc6f1a3e18bb38e447ac
sciencelover97 <53288573+sciencelover97@users.noreply.github.com>
2021-08-04 18:58:40 -0400

    Update Stage_0_KI.jl

--- Stage_0_KI.jl
+++ Stage_0_KI.jl
@@ -1,5 +1,12 @@
 Name = "Kamila Issabayeva" 
 Email = "kamila.issabayeva@nu.edu.kz "
 Slackname = "@Kamila"
+Twittername = "@Kissabayeva"
 Biostack = "Genomics"
-println("$Name , $Email , $Slackname , $Biostack .")
+dist = 0
+for i in 1:length(Slackname)
+    if Slackname[i] != Twittername[i] 
+        global dist += 1 
+    end  
+end        
+println("$Name , $Email , $Slackname , $Biostack , $dist .")
