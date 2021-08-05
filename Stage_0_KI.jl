5ea6fcf9e24cd9fa9b3632bf58060e97556cea66
sciencelover97 <53288573+sciencelover97@users.noreply.github.com>
2021-08-05 15:31:47 -0400

    Update Stage_0_KI.jl

--- Stage_0_KI.jl
+++ Stage_0_KI.jl
@@ -8,5 +8,8 @@ for i in 1:length(Slack_username)
     if Slack_username[i] != Twitter_handler[i] 
         global Hamming_dist += 1 
     end  
+    if length(Slack_username) != length(Twitter_handler)
+        global Hamming_dist += 1
+    end
 end        
 println("$Name , $Email , $Slack_username , $Twitter_handler , $Biostack , $Hamming_dist .")
