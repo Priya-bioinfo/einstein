7f758dee10f3becafd8a28b581a625ec07534f3c
Eshaan Agarwal <77532930+eshaan-nex@users.noreply.github.com>
2021-08-05 21:43:09 +0530

    Update trial.R

--- trial.R
+++ trial.R
@@ -1,8 +1,9 @@
+install.packages("DescTools")
 library(DescTools)
 name <- "Eshaan"
 email <- "eshaan.nex@gmail.com"
 slack_username <- "@eshaan  "
 biostack <- "drug discovery"
 twitter <- "EshaanNex"
-hamming_distance <- StrDist(slack_username, twitter, method = "hamming", ignore.case = TRUE, gap = 0)
+hamming_distance <- StrDist(slack_username, twitter, method = "hamming")
 cat(paste(name, email, trimws(slack_username), biostack, twitter, hamming_distance, sep = ","))
