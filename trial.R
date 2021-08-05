09eb400a25e06245322f3db4f12137a7a39bbfdf
Eshaan Agarwal <77532930+eshaan-nex@users.noreply.github.com>
2021-08-05 16:16:04 +0530

    Update trial.R

--- trial.R
+++ trial.R
@@ -4,6 +4,5 @@ email <- "eshaan.nex@gmail.com"
 slack_username <- "@eshaan  "
 biostack <- "drug discovery"
 twitter <- "EshaanNex"
-help("StrDist")
 hamming_distance <- StrDist(slack_username, twitter, method = "hamming", ignore.case = TRUE, gap = 0)
-cat(name, email, trimws(slack_username), biostack, twitter, hamming_distance)
+cat(paste(name, email, trimws(slack_username), biostack, twitter, hamming_distance, sep = ","))
