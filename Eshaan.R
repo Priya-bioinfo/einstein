52f933edfb885fe2cbb7c8310c1c0d8384a7a853
Eshaan Agarwal <77532930+eshaan-nex@users.noreply.github.com>
2021-08-06 15:32:26 +0530

    Update Eshaan.R

--- Eshaan.R
+++ Eshaan.R
@@ -2,8 +2,8 @@ install.packages("DescTools", verbose = FALSE, quiet = TRUE)
 suppressPackageStartupMessages(library(DescTools))
 name <- "Eshaan"
 email <- "eshaan.nex@gmail.com"
-slack_username <- "@eshaan  "
+slack_username <- "@eshaan   "
 biostack <- "drug discovery"
-twitter <- "EshaanNex"
+twitter <- "@EshaanNex"
 hamming_distance <- StrDist(slack_username, twitter, method = "hamming")
 cat(paste(name, email, trimws(slack_username), biostack, twitter, hamming_distance, sep = ","))
