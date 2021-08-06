7a4cda82c6c0024a165056ea2ca7eaf3a105dde9
Nisha6616 <72332145+Nisha6616@users.noreply.github.com>
2021-08-06 18:27:56 +0530

    Update stage0.sh

--- stage0.sh
+++ stage0.sh
@@ -3,6 +3,7 @@ git clone https://github.com/Priya-bioinfo/einstein.git
 cd einstein
 touch stage0.csv
 for f in *.R;do Rscript "$f">>stage0.csv;done
+for f in *.pl;do perl "$f">>stage0.csv;done
 cat stage0.csv
  
 
