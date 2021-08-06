6e292ad5952f4b1d5d7233cd8ae66aec888aeb59
Priya <66324139+Priya-bioinfo@users.noreply.github.com>
2021-08-06 18:16:05 +0530

    Update and rename Bukola888.md to Bukola888.py

---
+++ Bukola888.py
@@ -0,0 +1,17 @@
+name=("Bukola Omena-Okpowe")
+email=("bomena@caledonian.ac.uk")
+slackuserid=("bukola888")
+twitter=("@tbfabunmi")
+biostack=("drug discovery and development")
+
+s1='@bukola888'
+s2='@tbfabunmi'
+def h_distance(s1,s2):
+    h_distance = 0
+    for position in range (len(s1)):
+        if s1[position] !=s2[position]:
+            h_distance +=1
+        return h_distance
+
+
+print(name +"," +email + ","+slackuserid +","+biostack+"," +twitter +","+h_distance(s1, s2))
