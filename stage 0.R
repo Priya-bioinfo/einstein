cf716e621521c0cebcef7c7ebc6c10f83f50cabf
Yasmine <83483100+yasmine2021g@users.noreply.github.com>
2021-08-06 20:02:59 +0200

    Add files via upload

---
+++ stage 0.R
@@ -0,0 +1,11 @@
+name = "Yasmine Mohsen"
+Email_id = "yasmine.mohsen80@gmail.com"
+slack_user_name = "@Yasmine"
+twitter_handle = "@yasmineMohsen80 "
+Bio_stacks = ("transcriptomics and drug development")
+#hamming_distance
+x <- c(twitter_handle)
+y <- c(slack_user_name)
+sum(x != y )
+
+
