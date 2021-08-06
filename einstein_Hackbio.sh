5ecfd4a4cd17d024f33f2a22611eb0486c11a21f
Priya <66324139+Priya-bioinfo@users.noreply.github.com>
2021-08-06 21:16:58 +0530

    Update and rename stage0.sh to einstein_Hackbio.sh
    codes added

---
+++ einstein_Hackbio.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+git clone https://github.com/Priya-bioinfo/einstein.git
+cd einstein
+touch einstein_Hackbio.csv
+for f in *.R;do Rscript "$f">>einstein_Hackbio.csv;done
+for f in *.pl;do perl "$f">>einstein_Hackbio.csv;done
+for f in *py;do python "$f">>einstein_Hackbio.csv;done
+for f in *.php;do php "$f">>einstein_Hackbio.csv;done
+for f in *.jl;do julia "$f">>einstein_Hackbio.csv;done
+for f in *.c;do out1=${f:0:-2}
+		gcc $f -o $out1
+		./$out1 >>stage0.csv;done
+for f in *.java;do out1=${f:0:-5}
+		javac $f             
+		java $out1 >>stage0.csv;done
+for f in *.sh;do ./"$f">>einstein_Hackbio.csv;done
+cat stage0.csv
+ 
+
--- stage0.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-git clone https://github.com/Priya-bioinfo/einstein.git
-cd einstein
-touch stage0.csv
-for f in *.R;do Rscript "$f">>stage0.csv;done
-for f in *.pl;do perl "$f">>stage0.csv;done
-cat stage0.csv
- 
-
