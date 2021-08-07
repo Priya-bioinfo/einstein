395e3070261e0ceb845a3e5783587c7724889b39
Priya <66324139+Priya-bioinfo@users.noreply.github.com>
2021-08-07 13:05:04 +0530

    Update Stage_0_Mayaryasser.py

--- Stage_0_Mayaryasser.py
+++ Stage_0_Mayaryasser.py
@@ -7,8 +7,7 @@ twitter_handle = "@marotihsdkwe"
 def Hamm_dist(s1,s2):
 	ham=0
 	for i in range (len(s1)):
-    	
 		if s1[i] != s2 [i]:
 			ham=ham+1
-        return ham
+	return ham
 print( name+ ","+ email+ ","+slack_username + ","+biostack +","+ twitter_handle+","+str(Hamm_dist(slack_username,twitter_handle)))
