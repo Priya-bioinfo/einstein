84b3c145032d03c148ad46b1d60c00bc33e6c1fc
Eshaan Agarwal <77532930+eshaan-nex@users.noreply.github.com>
2021-08-07 03:26:04 -0700

    Update Stage_0_Eshaan.R

--- Stage_0_Eshaan.R
+++ Stage_0_Eshaan.R
@@ -1,7 +1,6 @@
 r = getOption("repos")
 r["CRAN"] = "http://cran.us.r-project.org"
 options(repos = r)
-install.packages("weatherData")
 install.packages("DescTools", verbose = FALSE, quiet = TRUE)
 suppressPackageStartupMessages(library(DescTools))
 name <- "Eshaan"
