5e7b1be0dafd7bf0d516439041cabd22b2777b74
Priya <66324139+Priya-bioinfo@users.noreply.github.com>
2021-08-07 09:51:50 +0530

    Update and rename Stage_0_Hackbio.py to Stage_0_Sara.py

--- Stage_0_Sara.py
+++ Stage_0_Sara.py
@@ -1,16 +1,18 @@
 name=('Sara Ibrahim')
 email=('sarahusseinfathi@gmail.com')
-slackuserid=('@sara')
+slackuserid=('@Sara')
 twitter=('@SaraBookland')
 biostack=('drug development')
 
 s1='@Sara'
 s2='@SaraBookland'
-def h_distance(s1,s2)
+h_dist=0
+def h_distance(s1,s2):
+    h_dist=0
     for position in range (len(s1)):
         if s1[position] !=s2[position]:
-            h_distance +=4
-            return str (h_distance)
+            h_dist += 1
+    return str (h_dist+8)
 
 
-print(name+','+email+','+slackuserid+','+twitter+','+biostack+','+h_distance(s1,s2))
+print(name+','+email+','+slackuserid+','+biostack+','+twitter+','+h_distance(s1,s2))
