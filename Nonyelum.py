e054d96acd48f27c386e5fb0d948d99d901ef176
Priya <66324139+Priya-bioinfo@users.noreply.github.com>
2021-08-07 11:33:38 +0530

    Update Nonyelum.py

--- Nonyelum.py
+++ Nonyelum.py
@@ -3,8 +3,17 @@ email = ("viviann.anakwenze@unizik.edu.ng")
 slackUsername = ("@Nonyelum")
 biostack = ("Genomics and Bioinformatics") 
 twitter_handle = ("@NonyelumAnakwe1")
-print (name, email, slackUsername, biostack, twitter_handle)
+
 x = slackUsername
 y = twitter_handle
-sum = int (y - x)
-hamming_distance = sum
+difference = len(y) - len(x)
+def hamming_distance (x,y):
+	for i in range(len(x)):
+		d=0
+		if x[i] == y [i]:
+			pass
+		else:
+			d =+ 1
+	d=d+difference
+	return d
+print (name+","+","+ email+","+ slackUsername+","+ biostack+","+ twitter_handle+","+str(hamming_distance (x,y)))
