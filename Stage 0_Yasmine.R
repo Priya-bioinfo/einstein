39a18f4ba2f5e2c495a63553ec5a1f524f2a96bc
Priya <66324139+Priya-bioinfo@users.noreply.github.com>
2021-08-07 10:13:54 +0530

    Update Stage 0_Yasmine.R

--- Stage 0_Yasmine.R
+++ Stage 0_Yasmine.R
@@ -1,11 +1,12 @@
 name = "Yasmine Mohsen"
 Email_id = "yasmine.mohsen80@gmail.com"
 slack_user_name = "@Yasmine"
-twitter_handle = "@yasmineMohsen80 "
+twitter_handle = "@yasmineMohsen80"
 Bio_stacks = ("transcriptomics and drug development")
 #hamming_distance
 x <- c(twitter_handle)
 y <- c(slack_user_name)
-sum(x != y )
+hamming_distance <- sum(x != y )
+cat(name,Email_id,slack_user_name,Bio_stacks,twitter_handle,hamming_distance,"\n",sep = ',')
 
 
