a4eafb464d32d846a1ef4906ae1db09a20ff6960
Priya <66324139+Priya-bioinfo@users.noreply.github.com>
2021-08-07 11:23:11 +0530

    Update Stage_0_Mayaryasser.py

--- Stage_0_Mayaryasser.py
+++ Stage_0_Mayaryasser.py
@@ -3,4 +3,11 @@ email = "mirayasser272@gmail.com"
 slack_username = "@marotihfkkwe"
 biostack = "geneomics"
 twitter_handle = "@marotihsdkwe"
-print( name+ ","+ email+ ","+slack_username + ","+biostack +","+ twitter_handle)
+
+def Hamm_dist(s1,s2):
+	for i in range (len(s1)):
+    ham=0
+		if s1[i] != s2 [i]:
+			ham=ham+1
+  return ham
+print( name+ ","+ email+ ","+slack_username + ","+biostack +","+ twitter_handle+","+str(Hamm_dist(slack_username,twitter_handle)))
