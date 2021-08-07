056b40bba38f5405e079953fe2a793ab94f90de7
Priya <66324139+Priya-bioinfo@users.noreply.github.com>
2021-08-07 07:49:53 +0530

    Update Simons_script-HD.py

--- Simons_script-HD.py
+++ Simons_script-HD.py
@@ -1,15 +1,15 @@
 Name = "Simone Salvador Boene"
-Email = " simonboene@gmail.com"
-Slackname = " @Sboene"
-Biostack = " Genomics"
+Email = "simonboene@gmail.com"
+Slackname = "@Sboene"
+Biostack = "Genomics"
 Handle_tweeter = "@SSBoene"
 str1 = "@Sboene"
 str2 = "@SSBoene"
 def hamming_distance(str1, str2):
 	hamming_distance = 0
-	for p in range (len(str1)):
+	for p in range(len(str1)):
 		if str1[p] != str2[p]:
-			hamming_distance =+1
+			hamming_distance += 1
 	if (len(str1)>len(str2)):
 		large=len(str1)
 		small=len(str2)
