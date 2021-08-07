2e3e97ac226d0e1a9056d80eb7398ae3ea1b5549
Nonyelum <88353544+Nonyelum@users.noreply.github.com>
2021-08-07 06:49:13 +0100

    Add files via upload

---
+++ Nonyelum.py
@@ -0,0 +1,10 @@
+name = ("Nonyelum Anakwenze")
+email = ("viviann.anakwenze@unizik.edu.ng")
+slackUsername = ("@Nonyelum")
+biostack = ("Genomics and Bioinformatics") 
+twitter_handle = ("@NonyelumAnakwe1")
+print (name, email, slackUsername, biostack, twitter_handle)
+x = slackUsername
+y = twitter_handle
+sum = int (y - x)
+hamming_distance = sum
