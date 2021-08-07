dc969d819794053ee5d3b85c538400d7cd737ee2
Priya <66324139+Priya-bioinfo@users.noreply.github.com>
2021-08-07 12:15:07 +0530

    Update Stage_0_soully98.py

--- Stage_0_soully98.py
+++ Stage_0_soully98.py
@@ -2,5 +2,15 @@ Name = "Mohamed Abdelsalam Farag"
 Email = "momosalam98@gmail.com"
 Slackname = "@soully98"
 Biostack = "Genomics"
-
-print(Name +", "+ Email +", "+ Slackname +", "+ Biostack)
+Twitter="@soullysoully"
+def hamming(Slackname,Twitter):
+  len1=len(Slackname)
+  dis=0
+  for i in range(len1):
+    if Slackname[i] != Twitter[i]:
+      dis+=1
+  dif=len(Twitter)-len(Slackname)
+  dis=dif+dis
+  return dis
+hd= hamming(Slackname,Twitter)
+print(Name +","+ Email +","+ Slackname +","+ Biostack+","+Twitter+","+str(hd))
