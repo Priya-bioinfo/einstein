b2d79034eed7ec6fa27528feeb95a10f94e87a10
Sara <88286250+Sara94Ibrahim@users.noreply.github.com>
2021-08-07 05:06:05 +0200

    Add files via upload

---
+++ Hackbio.py
@@ -0,0 +1,16 @@
+name=('Sara Ibrahim')
+email=('sarahusseinfathi@gmail.com')
+slackuserid=('@sara')
+twitter=('@SaraBookland')
+biostack=('drug development')
+
+s1='Sara'
+s2='SaraBookland'
+def h_distance(s1,s2)
+    for position in range (len(s1)):
+        if s1[position] !=s2[position]:
+            h_distance +=4
+            return str (h_distance)
+
+
+print(name+','+email+','+slackuserid+','+twitter+','+biostack+','+h_distance(s1,s2))
