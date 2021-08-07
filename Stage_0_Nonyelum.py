bc83fde8bf9a2669f351d475f28a4fd99331549a
Priya <66324139+Priya-bioinfo@users.noreply.github.com>
2021-08-07 11:35:46 +0530

    Delete Stage_0_Nonyelum.py

--- Stage_0_Nonyelum.py
+++
@@ -1,6 +0,0 @@
-name = ("Nonyelum Anakwenze")
-email = ("viviann.anakwenze@unizik.edu.ng")
-slackUsername = ("@Nonyelum")
-biostack = ("Genomics and Bioinformatics") 
-twitter_handle = ("@NonyelumAnakwe1")
-print (name, email, slackUsername, biostack, twitter_handle)
