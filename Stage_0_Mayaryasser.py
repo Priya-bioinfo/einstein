a452a12965db843d18018d9b1edec65f51b802c4
Mayar272 <88555357+Mayar272@users.noreply.github.com>
2021-08-07 07:43:13 +0200

    Add files via upload

---
+++ Stage_0_Mayaryasser.py
@@ -0,0 +1,6 @@
+name = "Mayar Yasser"
+email = "mirayasser272@gmail.com"
+slack_username = "@marotihfkkwe"
+biostack = "geneomics"
+twitter_handle = "@marotihsdkwe"
+print( name+ ","+ email+ ","+slack_username + ","+biostack +","+ twitter_handle)
