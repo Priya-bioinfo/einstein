26aa7e20faf196911dbb3dd1c3496acdfe06c708
Mayar272 <88555357+Mayar272@users.noreply.github.com>
2021-08-07 17:36:54 +0200

    Add files via upload

---
+++ Stage_0_Mayarshahin.py
@@ -0,0 +1,6 @@
+name = "Mayar Mohamed"
+email = "mayar1999.mohamed30@gmail.com"
+slack_username = "@Mayar"
+biostack = "geneomics and medicinal chemistry"
+twitter_handle = "@MayarMo03151528"
+print( name+ ","+ email+ ","+slack_username + ","+biostack +","+ twitter_handle)
