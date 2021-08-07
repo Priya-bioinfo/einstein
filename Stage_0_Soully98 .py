927147ed0310672ed419cac0d677ac8876f8a007
soully98 <88315252+soully98@users.noreply.github.com>
2021-08-07 12:41:58 +0200

    Add files via upload

---
+++ Stage_0_Soully98 .py
@@ -0,0 +1,15 @@
+Name = "Mohamed Abdelsalam Farag"
+Email = "momosalam98@gmail.com"
+Slackname = "@soully98"
+twitter= "soully98"
+Biostack = "Genomics"
+
+s1="soully98"
+s2="soully98"
+def h_distance(s1,s2):
+    h_distance = 0
+    for position in range (len(s1)):
+        if s1[position] !=s2[position]:
+            h_distance +=1
+    return str(h_distance)
+print(name +"," +email + ","+slackuserid +","+biostack+"," +twitter +","+h_distance(s1, s2))
