9372c79903fd9d5c99ee436a0d8503df01505ee5
Priya <66324139+Priya-bioinfo@users.noreply.github.com>
2021-08-07 09:08:51 +0530

    Update Stage_0_XR.c

--- Stage_0_XR.c
+++ Stage_0_XR.c
@@ -1,6 +1,30 @@
 #include <stdio.h>
+#include <string.h>
 int main() 
 { 
-printf("NAME: BHUSHAN WAGH \nEmail: waghbhushan1123@gmail.com\nLanguage_used: C_Program\nBiostack: Software Development \nSlack_username: @XR"); 
-return 0;
+
+	char str1[20] = "@XR";
+	char str2[20] = "@XRfinder";
+	int len, differe, small, big;
+
+	if ( strlen(str1) < strlen(str2) )
+	{
+		small = strlen(str1);
+		big = strlen(str2);
+		}
+	else
+	{
+		small = strlen(str2);
+		big = strlen(str1);
+		}
+	int i = 0, hd = 0;
+	for (i=0; i < small; i++)
+  	{
+        	if (str1[i] != str2[i])
+           	    hd++;
+        }
+	differe = big-small;
+	hd = hd+differe;
+	printf("BHUSHAN WAGH,waghbhushan1123@gmail.com,@XR,Software Development,@XRfinder,%d\n",hd); 
+	return 0;
 }
